b3562504ff7cd308d47d497ba5e7516ede61e898
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-23 19:20:57 +0700

    first

--- creating_list_from_lists.py
+++ creating_list_from_lists.py
@@ -1,4 +1,7 @@
-# Use list comprehension to create a new list
+def create_new_list(list1, list2):
+    # Use list comprehension to create a new list
+    result_list = [num for num in list1 if num % 2 != 0] + [num for num in list2 if num % 2 == 0]
+    return result_list
 # Given lists
 # Create a new list based on the conditions
 # Print the result
